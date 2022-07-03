101d01dbf220d695f0daf460da8b87efeb2ad4a7
VictorTsai <victor.tsai@tpisoftware.com>
2022-07-03 11:22:42 +0800

    initial commit

---
+++ src/main/java/idv/victor/web/OfficialWebsiteApplication.java
@@ -0,0 +1,13 @@
+package idv.victor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialWebsiteApplication.class, args);
+	}
+
+}
