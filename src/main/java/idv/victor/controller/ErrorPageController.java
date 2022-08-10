fbf87c0f1676fd5f0aac90ec188302cfc575db7a
VictorTsai <victor.tsai@tpisoftware.com>
2022-08-10 23:03:09 +0800

    error page Handling 404, 500 error

---
+++ src/main/java/idv/victor/controller/ErrorPageController.java
@@ -0,0 +1,35 @@
+package idv.victor.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Handling Different Error in Web
+ */
+@Controller
+public class ErrorPageController implements ErrorController {
+    @RequestMapping(value = "/error")
+    public String errorPagePath(HttpServletRequest request){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "errorPage-404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "errorPage-500";
+            }
+        }
+        return "errorPage";
+    }
+
+}
